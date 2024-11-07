5465ae2b339198909e03dd218aa6e5cfa97bb691
jorge garcia <jorge.garcia.celis@gmail.com>
2024-11-07 17:45:04 -0600

    grid

--- src/PaymentPage.jsx
+++ src/PaymentPage.jsx
@@ -1,20 +1,26 @@
 import React, { useState } from 'react';
 import PaymentInput from './PaymentInput';
 import PaymentTable from './PaymentTable';
-import  Grid  from '@mui/material/Grid2';
+import Grid from '@mui/material/Grid2';
+import Container from '@mui/material/Container'
+
 
 export default function PaymentPage() {
     const [formData, setFormData] = useState([]);
 
     return (
         <>
-            <Grid container spacing={1}>
+        <Container>
+
+            <Grid container columnSpacing={4} sx={{marginTop: 4}}>
                 <Grid size={3}>
                     <PaymentInput formData={formData} setFormData={setFormData} />
                 </Grid>
                 <Grid size={9}>
                     <PaymentTable formData={formData} /></Grid>
             </Grid>
+        </Container>
+
         </>
     );
 }
